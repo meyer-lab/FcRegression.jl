534d47cb89e1b6a5a7f6c4857b1e48b9b3814a7b
Cyrillus Tan <cyztan@gmail.com>
2019-11-26 20:58:34 -0800

    Regression on data (#19)
    * Function to do regression with actual data
    
    Tested: exponential works, Gompertz doesn't. Fitting with L0 and f will crush Req_Regression. Need to tune curve_fit
    Wondering if curve_fit is the best way to fit L0 and f
    Also, how do we handle concentration from the data?
    
    * Update dataHelpers.jl
    
    * small fix on murine FcgR
    
    * Fix concentration and cell KO cases

--- src/dataHelpers.jl
+++ src/dataHelpers.jl
@@ -23,7 +23,8 @@ function geocmean(x)
 end
 
 cellTypes = [:ncMO, :cMO, :NKs, :Neu, :EO]
-murine_FcgR = [:FcgRI, :FcgRIIB, :FcgRIII, :FcgRIV]
+murineFcgR = [:FcgRI, :FcgRIIB, :FcgRIII, :FcgRIV]
+murineActI = [1, -1, 1, 1]
 
 function importRtot(; murine=true)
     if murine
