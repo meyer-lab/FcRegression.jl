eece6abb2c4d4637cc50c4bdb4ec647b8a288047
Micah Bryant <micah.bryant14@gmail.com>
2019-12-01 14:14:33 -0800

    completed test function

--- test/testSynergy.jl
+++ test/testSynergy.jl
@@ -26,8 +26,10 @@
     @testset "test synergyGrid" begin
         Kav = ones(6, 5) * 1e9
         FcExpr = ones(5) * 1e3
-        #the grid should be 5x5
-        grid = synergyGrid(4, random.random(), FcExpr, Kav) #not sure if want to keep random functionality
-        
+        #the grid should be 6x6
+        grid = synergyGrid(4, 1.2e-9, FcExpr, Kav) #not sure if want to keep random functionality
+        shape = (size(Kav)[1], size(Kav)[1])
+        @test size(grid) = shape
+        @test grid = transpose(grid)
     end
 end
