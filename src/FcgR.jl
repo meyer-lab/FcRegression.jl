26c941f7e38036ae8b41caebe6b286298cd93faa
Aaron Meyer <aaron@asmlab.org>
2019-12-26 08:54:18 -0800

    Try this.

--- src/FcgR.jl
+++ src/FcgR.jl
@@ -5,6 +5,9 @@ include("dataHelpers.jl")
 include("regression.jl")
 include("synergy.jl")
 include("translation.jl")
+
+using Plots
+
 include("figures/figureB1.jl")
 
 export calculateIsobologram, polyfc, polyfc_ActV
