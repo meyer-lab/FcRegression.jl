2f240fbf2fcc50830435a1b097f9d391326a8ca7
Robby <robcatdude@yahoo.com>
2020-01-13 20:07:18 -0800

    Add Receptor Names

--- test/testDataHelpers.jl
+++ test/testDataHelpers.jl
@@ -4,21 +4,25 @@
         arr_heterozygote = FcgR.importRtot(murine=false, genotype="ZZZ")
         arr_RTF = FcgR.importRtot(murine=false, genotype="RTF")
         arr_HZF = FcgR.importRtot(murine=false, genotype="HZF")
+        
+        ## Set up mapping
+        receps = ["FcgRI", "FcgRIIA-131H", "FcgRIIA-131R", "FcgRIIB-232I", "FcgRIIB-232T", "FcgRIIC-13N", "FcgRIIIA-158V", "FcgRIIIA-158F", "FcgRIIIB"]
+        idxs = 1:9
+        d = Dict(receps.=> idxs)
 
         ## Assert correct assigning of the genotypes (can add reverse cases too)
-        @test(arr_HIV[2, :] == arr_RTF[3, :] && arr_HIV[3, :] == arr_RTF[2, :])
-        @test(arr_HIV[4, :] == arr_RTF[5, :] && arr_HIV[5, :] == arr_RTF[4, :])
-        @test(arr_HIV[7, :] == arr_RTF[8, :] && arr_HIV[8, :] == arr_RTF[7, :])
+        @test(arr_HIV[d["FcgRIIA-131H"], :] == arr_RTF[d["FcgRIIA-131R"], :] && arr_HIV[d["FcgRIIA-131R"], :] == arr_RTF[d["FcgRIIA-131H"], :])
+        @test(arr_HIV[d["FcgRIIB-232I"], :] == arr_RTF[d["FcgRIIB-232T"], :] && arr_HIV[d["FcgRIIB-232T"], :] == arr_RTF[d["FcgRIIB-232I"], :])
+        @test(arr_HIV[d["FcgRIIIA-158V"], :] == arr_RTF[d["FcgRIIIA-158F"], :] && arr_HIV[["FcgRIIIA-158F"], :] == arr_RTF[d["FcgRIIIA-158V"], :])
 
         ## Check Heterozygote Case
-        @test(arr_HIV[2, :] == 2 .* arr_heterozygote[2, :] && arr_HIV[2, :] == 2 .* arr_heterozygote[3, :])
-        @test(arr_RTF[3, :] == 2 .* arr_heterozygote[2, :] && arr_RTF[3, :] == 2 .* arr_heterozygote[3, :])
-        @test(arr_HIV[4, :] == arr_heterozygote[4, :] .+ arr_heterozygote[5, :])
-        @test(arr_RTF[5, :] == arr_heterozygote[4, :] .+ arr_heterozygote[5, :])
+        @test(arr_RTF[d["FcgRIIA-131H"], :] == arr_heterozygote[d["FcgRIIA-131H"], :] .+ arr_heterozygote[d["FcgRIIA-131R"], :])
+        @test(arr_HIV[d["FcgRIIB-232I"], :] == arr_heterozygote[d["FcgRIIB-232I"], :] .+ arr_heterozygote[d["FcgRIIB-232T"], :])
+        @test(arr_RTF[d["FcgRIIIA-158F"], :] == arr_heterozygote[d["FcgRIIIA-158V"], :] .+ arr_heterozygote[d["FcgRIIIA-158F"], :])
 
         ## Check mixed genotype case
-        @test(arr_HZF[2, :] == arr_HIV[2, :])
-        @test(arr_HZF[4, :] == arr_HZF[5, :])
-        @test(arr_RTF[5, :] == arr_HZF[4, :] .+ arr_HZF[5, :])
+        @test(arr_HZF[d["FcgRIIA-131H"], :] == arr_HIV[d["FcgRIIA-131H"], :])
+        @test(arr_HZF[d["FcgRIIB-232I"], :] == arr_HZF[d["FcgRIIB-232T"], :])
+        @test(arr_RTF[d["FcgRIIB-232T"], :] == arr_HZF[d["FcgRIIB-232I"], :] .+ arr_HZF[d["FcgRIIB-232T"], :])
     end
 end
