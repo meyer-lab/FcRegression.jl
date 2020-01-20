76dc78313834bea1d81e160104a0e4a907366fcf
stephendl102 <41273188+stephendl102@users.noreply.github.com>
2020-01-19 17:31:06 -0800

    Fig b1 (#66)
    * Add PlotSynValency function
    
    * Add IgG2bFucose to Plots
    
    * Corrected the synergy plots
    
    * edit
    
    * Change plotIsobologramTwo to display IgG2bFucose
    
    * Update figureB1.jl
    
    * Set IgG2bFucose to true

--- src/figures/figureB1.jl
+++ src/figures/figureB1.jl
@@ -23,20 +23,20 @@ end
 
 """ Plot an example isobologram. """
 function plotIsobologramTwo()
-    Kav = importKav(murine = true)
+    Kav = importKav(murine = true, IgG2bFucose = true)
     # TODO: Should import actual receptor abundance
     FcExpr = importRtot(murine = true)[:, 2]
 
-    output = calculateIsobologram(2, 3, 4, 1.0e-9, FcExpr, Kav, actV = murineActI)
+    output = calculateIsobologram(2, 5, 4, 1.0e-9, FcExpr, Kav, actV = murineActI)
     output /= maximum(output)
 
     X = range(0, stop = 1, length = length(output))
 
     pl = plot(X, output, title = "Activity vs IgG Composition", xticks = false, legend = false)
     plot!(pl, [0, 1], [output[1], output[33]])
-    annotate!(pl, [(0, 0, text("100% mIgG2a", 8, :right, rotation = 45)), (1.0, 0, text("100% mIgG2b", 8, :right, rotation = 45))])
+    annotate!(pl, [(0, 0, text("100% mIgG2a", 8, :right, rotation = 45)), (1.0, 0, text("100% mIgG2bFucose", 8, :right, rotation = 45))])
     ylabel!(pl, "cMO Predicted Activity")
-    xlabel!(pl, "Percent mIgG2b")
+    xlabel!(pl, "Percent mIgG2bFucose")
     ylims!(pl, (-0.02, maximum(output) * 1.1))
 
     return pl
@@ -44,7 +44,7 @@ end
 
 """Figure shows the affect of increasing immune complex concentration on synergies for each IgG combination"""
 function PlotSynGraph()
-    Kav = importKav(murine = true)
+    Kav = importKav(murine = true, IgG2bFucose = true)
     FcgR = importRtot(murine = true)[:, 2] #2 = mean cMO
     IC = exp10.(range(-12, stop = -6, length = 20))
     S = zeros((length(IC), 10))
@@ -52,10 +52,10 @@ function PlotSynGraph()
     for (ii, value) in enumerate(IC)
         A = synergyGrid(4, value, FcgR, Kav)
         h = collect(Iterators.flatten(A))
-        S[ii, 1:4] = h[1:4]
-        S[ii, 5:7] = h[6:8]
-        S[ii, 8:9] = h[11:12]
-        S[ii, 10] = h[16]
+        S[ii, 1:5] = h[2:6]
+        S[ii, 5:8] = h[8:11]
+        S[ii, 8:9] = h[14:15]
+        S[ii, 10] = h[20]
     end
 
     pl = plot(
@@ -63,12 +63,42 @@ function PlotSynGraph()
         S,
         xaxis = :log,
         title = "Effect of Concentration on Synergy",
-        label = ["IgG1/2a" "IgG1/2b" "IgG1/3" "IgG2a/2b" "IgG2a/3" "IgG2b/3"],
+        label = ["IgG1/2a" "IgG1/2b" "IgG1/3" "IgG1/2b-Fucose" "IgG2a/2b" "IgG2a/3" "IgG2a/2b-Fucose" "IgG2b/3" "IgG2b/2b-Fucose" "IgG3/2b-Fucose"],
         legend = :topleft,
     )
     xlabel!(pl, "IC Concentration")
     ylabel!(pl, "Synergy")
 
+    return pl
+end
+
+    """ Figure shows how immune complex valency affects synergy """
+function PlotSynValency()
+    Kav = importKav(murine = true, IgG2bFucose = true)
+    FcgR = importRtot(murine = true)[:, 2] #2 = mean cMO
+    IC = 10e-9
+    Valency = range(1, stop = 24)
+    S = zeros((length(Valency), 10))
+        
+    for (ii, value) in enumerate(Valency)
+        A = synergyGrid(value, IC, FcgR, Kav)
+        h = collect(Iterators.flatten(A))
+        S[ii, 1:5] = h[2:6]
+        S[ii, 5:8] = h[8:11]
+        S[ii, 8:9] = h[14:15]
+        S[ii, 10] = h[20]
+    end
+    
+    pl = plot(
+        Valency,
+        S,
+        title = "Effect of IC Valency on Synergy",
+        label = ["IgG1/2a" "IgG1/2b" "IgG1/3" "IgG1/2b-Fucose" "IgG2a/2b" "IgG2a/3" "IgG2a/2b-Fucose" "IgG2b/3" "IgG2b/2b-Fucose" "IgG3/2b-Fucose"],
+        legend = :topleft,
+    )
+    xlabel!(pl, "IC Valency")
+    ylabel!(pl, "Synergy")
+    
     return pl
 end
 
