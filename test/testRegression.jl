ea0e10b41329debd1d860162b5535d8b6becd73a
cztan <cyztan@gmail.com>
2020-01-23 18:32:23 -0800

    LOOCV, Bootstrap, proportion loss
    Will explain in the upcoming meeting

--- test/testRegression.jl
+++ test/testRegression.jl
@@ -2,9 +2,16 @@
 
 @testset "Check that all combinations of the actual regression converge." begin
     for data in ("ITP", "melanoma", "blood", "bone")
-        for method in (FcgR.exponential, FcgR.weibull)
-            fit = FcgR.fitRegression(data, method)
-            @test Optim.converged(fit)
+        fit = FcgR.fitRegression(FcgR.importDepletion(data), FcgR.proportion_loss)
+        @test Optim.converged(fit)
+    end
+end
+
+@testset "Test cross validation and bootstrapping." begin
+    for data in ("ITP", "melanoma", "blood", "bone")
+        for loss in (FcgR.quadratic_loss, FcgR.proportion_loss)
+            @test all(Optim.converged.(FcgR.LOOCrossVal(data, loss)))
+            @test all(Optim.converged.(FcgR.bootstrap(data, loss)))
         end
     end
 end
