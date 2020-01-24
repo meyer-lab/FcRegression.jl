73ba29fefcaa9183d4cf5cd087d9ef3eae2c5e86
Cyrillus Tan <cyztan@gmail.com>
2020-01-24 14:28:43 -0800

    Exponential regression with proportion loss function (#74)
    * Setup.
    
    * Tweaks.
    
    * Try this.
    
    * Change to general fitting.
    
    * Might be setup.
    
    * Working.
    
    * Working now.
    
    * Remove Calculus.jl
    
    * OCD
    
    * LOOCV, Bootstrap, proportion loss
    
    Will explain in the upcoming meeting
    
    * some updates
    
    * Small change
    
    Co-authored-by: Aaron Meyer <2065146+aarmey@users.noreply.github.com>

--- src/regression.jl
+++ src/regression.jl
@@ -1,3 +1,5 @@
+import MLBase.LOOCV
+import StatsBase.sample
 
 exponential(X::Matrix, p::Vector) = Distributions.cdf.(Distributions.Exponential(), X * p)
 weibull(X::Matrix, p::Vector) = Distributions.cdf.(Distributions.Weibull(p[1]), X * p[2:end])
@@ -35,37 +37,73 @@ function regGenData(df; L0, f, KxStar = KxConst, murine = true)
 end
 
 
-function loss_wL0f(df, ps::Vector{T}, regMethod::Function)::T where {T <: Real}
-    (X, Y) = regGenData(df; L0 = 10.0^ps[1], f = ps[2])
-    return Distances.sqeuclidean(regMethod(X, ps[3:end]), Y)
+function quadratic_loss(X::Matrix, w::Vector, Y::Vector)
+    return Distances.sqeuclidean(exponential(X, w), Y)
+end
+
+function proportion_loss(X::Matrix, w::Vector, Y::Vector)
+    """
+    log(λ_i) = w'* x_i
+    T ~ exp(λ_i)
+    """
+    p = -expm1.(-exp.(X * w))
+    return sum((Y.-p).^2 ./ (p.*(1 .-p)))
 end
 
 
-function fitRegression(dataType, regMethod::Function)
-    df = importDepletion(dataType)
+function loss_wL0f(df, ps::Vector{T}, lossFunction::Function)::T  where {T<:Real}
+    (X, Y) = regGenData(df; L0 = 10.0^ps[1], f = ps[2])
+    return lossFunction(X, ps[3:end], Y)
+end
+
+
+function fitRegression(df, lossFunction::Function; wL0f = false)
+    ## this method only supports expoential distribution due to param choice
+
     (X, Y) = regGenData(df; L0 = 1.0e-9, f = 4)
     Np = size(X, 2)
-
-    fitMethod = (ps) -> loss_wL0f(df, ps, regMethod)
+    if wL0f
+        fitMethod = (ps) -> loss_wL0f(df, ps, lossFunction)
+    else
+        fitMethod = (ps) -> proportion_loss(X, ps, Y)
+    end
     g! = (G, ps) -> ForwardDiff.gradient!(G, fitMethod, ps)
 
-    p_init = 0.1 * ones(Float64, Np)
-    p_lower = zeros(Float64, Np)
-    p_upper = ones(Float64, Np)
+    p_init = zeros(Float64, Np)
+    p_lower = - 100 .* ones(Float64, Np)
+    p_upper = 100 .* ones(Float64, Np)
 
-    if regMethod == weibull
-        p_init = [1.0; p_init]
-        p_lower = [0.1; p_lower]
-        p_upper = [10; p_upper]
+    if wL0f
+        p_init = vcat(-9, 4, p_init)
+        p_lower = vcat(-16, 1, p_lower)
+        p_upper = vcat(-7, 6, p_upper)
     end
-
-    p_init = vcat(-9, 4, p_init)
-    p_lower = vcat(-16, 1, p_lower)
-    p_upper = vcat(-7, 6, p_upper)
 
     fit = optimize(fitMethod, g!, p_lower, p_upper, p_init, Fminbox())
     if !Optim.converged(fit)
         @warn "Fitting did not converge"
     end
     return fit
+end
+
+function LOOCrossVal(dataType, lossFunction::Function)
+    df = importDepletion(dataType)
+    n = size(df, 1)
+    fitResults = Vector(undef, n)
+    LOOindex = LOOCV(n)
+    for (i, idx) in enumerate(LOOindex)
+        fitResults[i] = fitRegression(df[idx, :], lossFunction)
+    end
+    return fitResults
+end
+
+
+function bootstrap(dataType, lossFunction::Function; nsample = 100)
+    df = importDepletion(dataType)
+    n = size(df, 1)
+    fitResults = Vector(undef, nsample)
+    for i in 1:nsample
+        fitResults[i] = fitRegression(df[sample(1:n, n, replace = true), :], lossFunction)
+    end
+    return fitResults
 end
