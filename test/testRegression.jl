f5ee170dead8fa3cf4ede8870f03e9d6e5ce3945
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-01-26 09:10:00 -0800

    Shuffle test (#85)
    * Randomly shuffle dataframe rows.
    
    * Remove print.
    
    * Update testRegression.jl

--- test/testRegression.jl
+++ test/testRegression.jl
@@ -13,3 +13,15 @@ end
     @test all(Optim.converged.(FcgR.LOOCrossVal("ITP", FcgR.proportion_loss)))
     @test all(Optim.converged.(FcgR.bootstrap("ITP", FcgR.proportion_loss)))
 end
+
+@testset "Test for similar results with row shuffling." begin
+	dataA = FcgR.importDepletion("ITP")
+	dataB = dataA[shuffle(1:size(dataA, 1)), :]
+
+	fitA = FcgR.fitRegression(dataA, FcgR.proportion_loss)
+	fitB = FcgR.fitRegression(dataB, FcgR.proportion_loss)
+
+	@test Optim.converged(fitA)
+	@test Optim.converged(fitB)
+	@test Optim.minimum(fitA) ≈ Optim.minimum(fitB)
+end
