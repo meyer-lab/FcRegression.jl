385f8acc0285eda5d0407d14588941404c47d177
cztan <cyztan@gmail.com>
2020-02-14 13:06:13 -0800

    Cross Validation Predictions

--- test/testRegression.jl
+++ test/testRegression.jl
@@ -1,6 +1,6 @@
 """ Generate simulated Y given X and parameters. """
 
-@testset "Check that all combinations of the actual regression converge." begin
+@testset "Regression: Check that all combinations of the actual regression converge." begin
     for data in ("ITP", "melanoma", "blood", "bone")
         for loss in (FcgR.quadratic_loss, FcgR.proportion_loss)
             fit = FcgR.fitRegression(FcgR.importDepletion(data), loss)
@@ -9,12 +9,12 @@
     end
 end
 
-@testset "Test cross validation and bootstrapping." begin
+@testset "Regression: Test cross validation and bootstrapping." begin
     @test all(Optim.converged.(FcgR.LOOCrossVal("ITP", FcgR.proportion_loss)))
     @test all(Optim.converged.(FcgR.bootstrap("ITP", FcgR.proportion_loss)))
 end
 
-@testset "Test for similar results with row shuffling." begin
+@testset "Regression: Test for similar results with row shuffling." begin
     dataA = FcgR.importDepletion("ITP")
     dataB = dataA[shuffle(1:size(dataA, 1)), :]
 
