68b91d2221a93147550c8304de59b6ef673585a2
Robby <robcatdude@yahoo.com>
2020-02-18 00:04:12 -0800

    Figure Resize

--- src/figures/figureB5.jl
+++ src/figures/figureB5.jl
@@ -2,6 +2,6 @@
 function figureB5()
     p1 = plotActualvFit("blood")
     p2 = plotActualvPredict("blood")
-    p = plot(p1, p2, layout = (1, 2), size = (600, 1200), dpi = 300)
+    p = plot(p1, p2, layout = (1, 2), size = (1200, 600), dpi = 300)
     savefig(p, "figureB5.pdf")
 end
