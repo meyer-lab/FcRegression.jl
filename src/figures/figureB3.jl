f16ef6ac8c51c925d9d13521905564316227768f
Aaron Meyer <git@asmlab.org>
2020-03-04 14:16:03 -0800

    Fix.

--- src/figures/figureB3.jl
+++ src/figures/figureB3.jl
@@ -1,7 +1,7 @@
 """ This file builds the depletion manuscript, Figure 3 (melanoma). """
 
 function figureB3()
-    p1, p2, p3, p4 = figureW("melonoma"; L0 = 1e-9, f = 4)
+    p1, p2, p3, p4 = figureW("melanoma"; L0 = 1e-9, f = 4)
 
     draw(PDF("figureB3.pdf", 1000px, 800px), gridstack([p1 p2; p3 p4]))
 end
