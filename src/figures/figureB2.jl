1c71fd551d5c063dd2d9236501d947431746d2c6
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-03-08 10:39:07 -0700

    Update figureB2.jl

--- src/figures/figureB2.jl
+++ src/figures/figureB2.jl
@@ -9,26 +9,26 @@ end
 function figureB3()
     p1, p2, p3, p4 = figureW("ITP"; L0 = 1e-9, f = 4)
 
-    draw(PDF("figureB3.pdf", 1000px, 800px), gridstack([p1 p2; p3 p4]))
+    draw(SVG("figureB3.svg", 1000px, 800px), gridstack([p1 p2; p3 p4]))
 end
 
 """ This file builds the depletion manuscript, Figure 4 (blood). """
 function figureB4()
     p1, p2, p3, p4 = figureW("blood"; L0 = 1e-9, f = 4)
 
-    draw(PDF("figureB4.pdf", 1000px, 800px), gridstack([p1 p2; p3 p4]))
+    draw(SVG("figureB4.svg", 1000px, 800px), gridstack([p1 p2; p3 p4]))
 end
 
 """ This file builds the depletion manuscript, Figure 5 (bone). """
 function figureB5()
     p1, p2, p3, p4 = figureW("bone"; L0 = 1e-9, f = 4)
 
-    draw(PDF("figureB5.pdf", 1000px, 800px), gridstack([p1 p2; p3 p4]))
+    draw(SVG("figureB5.svg", 1000px, 800px), gridstack([p1 p2; p3 p4]))
 end
 
 """ This file builds the depletion manuscript, Figure 6 (HIV). """
 function figureB6()
     p1, p2, p3, p4 = figureW("HIV"; L0 = 1e-9, f = 4)
 
-    draw(PDF("figureB5.pdf", 1000px, 800px), gridstack([p1 p2; p3 p4]))
+    draw(SVG("figureB5.svg", 1000px, 800px), gridstack([p1 p2; p3 p4]))
 end
