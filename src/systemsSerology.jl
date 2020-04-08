e7de74f88c6d2d0b82e17927654f21fb608df556
Madeleine <murphym21@g.ucla.edu>
2020-04-07 22:20:45 -0700

    cleanup Plot function

--- src/systemsSerology.jl
+++ src/systemsSerology.jl
@@ -13,37 +13,13 @@ end
 
 """ Plot HIV1.p66 data in terms of FcgRIIIa vs. ADCC data"""
 function plotHIV1p66()
-    dataDir = joinpath(dirname(pathof(FcgR)), "..", "data")
-    dfF = CSV.read(joinpath(dataDir, "alter-MSB", "data-function.csv"))
-    dfL = HIV1p66sub()
+    df1 = FcgR.antigenTables("HIV1.p66") 
+    final = rename!(df1, :FcgRIIIa_F158 => :F158, :FcgRIIIa_V158 =>  :V158)
     
-    # Gather all of the ADCC data from "data-function.csv"
-    newdfF = dfF[dfF[!, :ADCC].!= "NA", :]
-    ADCClist = [parse(Float64,x) for x in newdfF[!, :ADCC]] # All ADCC data in one array of ints
-    dfADCC = newdfF[:, [:Column1, :ADCC]]
-    rename!(dfADCC, [:Subject, :ADCC])
-    
-    # Note what subjects correspond to the ADCC data
-    Subjects = dfADCC[!, :Subject]
-    
-    #Find corresponding data for FcgRIIIa...HIV1.p66:
-  
-    #find all rows that contain data for FcgRIIIa.F158.6H.HIV1.p66 & FcgR.IIIa.V158.6H.HIV1.p66, 
-    #and create new subsetted dataframes    
-    newdfL = dfL[occursin.("FcgRIIIa", dfL.Rec), :]
-    newdfLF = newdfL[occursin.("F158", newdfL.Vir), :]
-    newdfLV = newdfL[occursin.("V158", newdfL.Vir), :]
-    
-    #do not use subjects in FcgrIIIa table who had no corresponding ADCC data
-    F = newdfLF[(newdfLF.Subject .!= 615167) .& (newdfLF.Subject .!= 930173), :]
-    V = newdfLV[(newdfLV.Subject .!= 615167) .& (newdfLV.Subject .!= 930173), :]
-    
-    #Create combined dataframe
-    final = DataFrame(Subject = Subjects, ADCC = ADCClist, F158 = F[!, :Value], V158 = V[!, :Value])
-    
-    #using StatsPlots
-    # Reimplement in Gadfly
-    # @df final scatter([:F158 :V158], :ADCC, xlabel = "FcgRIIIA Value", ylabel = "ADCC", title = "HIV1.p66")
+     #using StatsPlots - New Note: Assuming Gadfly plots can fix this? The ADCC values will plot in list order(not in increasing 
+                        #order) along y axis with this simplification of the function
+     # Reimplement in Gadfly 
+     # @df final scatter([:F158 :V158], :ADCC, xlabel = "FcgRIIIA Value", ylabel = "ADCC", title = "HIV1.p66")
 end
 
 
@@ -56,7 +32,7 @@ function antigenTables(string)
     
     #Treat each genetic variant as a different receptor (add the genetic variant to overall receptor name in rec column)
     for i=1:size(df, 1)
-        df.Rec[i] = df.Rec[i] * "." * df.Vir[i]
+        df.Rec[i] = df.Rec[i] * "_" * df.Vir[i]
     end
     
     rec = unstack(df, :Subject, :Rec, :Value) #stack all the receptors as columns
