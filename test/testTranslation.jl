904ecde38a8b51e3038effd854a54a6964c06aff
ctan <cyztan@gmail.com>
2020-04-27 10:25:08 -0700

    Adjust test file

--- test/testTranslation.jl
+++ test/testTranslation.jl
@@ -1,7 +1,7 @@
 @testset "translation.jl tests" begin
     @testset "Test that more refined brute force improves match." begin
-        resultTwo = FcgR.brute_force_discrete(1)
-        resultFour = FcgR.brute_force_discrete(3)
+        resultTwo = FcgR.brute_force_optimum(1)
+        resultFour = FcgR.brute_force_optimum(3)
 
         @test resultTwo[1] <= resultFour[1]
     end
