c3b3541c7fc08826cc123ee939510705dc935b26
stephendl102 <41273188+stephendl102@users.noreply.github.com>
2020-06-03 18:22:06 -0700

    Update fcBindingModel.jl

--- src/fcBindingModel.jl
+++ src/fcBindingModel.jl
@@ -4,24 +4,20 @@ function polyfc_ActV(L0, KxStar, f, Rtot::Array, IgGC::Array, Kav::AbstractMatri
     Input:
     Rtot: nr * nct matrix, nct = # cell types
     IgGC: ni * nset matrix, nset = # IgGC combinations
-    Mix: Specifies that the output will be a single IgG response if mix = false 
+    Mix: Specifies that output will be the response to a single IgG input if mix = false 
     Output:
     Matrix of size nct * nset filled with ActV
     """
     ansType = promote_type(typeof(L0), typeof(KxStar), typeof(f), eltype(Rtot), eltype(IgGC))
     res = Matrix{ansType}(undef, size(Rtot, 2), size(IgGC, 2))
     if Mix
-        for ict = 1:size(res, 1)
-            for iset = 1:size(res, 2)
-                res[ict, iset] = polyfc(L0, KxStar, f, Rtot[:, ict], IgGC[:, iset], Kav, ActI).ActV
-            end
-        end
+        L0 = ones(size(res, 2))*L0
     else
-        L = (range(0, stop = 1, length = size(res, 2)))*L0
-        for ict = 1:size(res, 1)
-            for iset = 1:size(res, 2)
-                res[ict, iset] = polyfc(L[iset], KxStar, f, Rtot[:, ict], IgGC[:, iset], Kav, ActI).ActV
-            end
+        L0 = (range(0, stop = 1, length = size(res, 2)))*L0
+    end
+    for ict = 1:size(res, 1)
+        for iset = 1:size(res, 2)
+            res[ict, iset] = polyfc(L0[iset], KxStar, f, Rtot[:, ict], IgGC[:, iset], Kav, ActI).ActV
         end
     end
     return res
