1276c40b81e8f4359b0f2a5dade703e23c399bbb
ctan <cyztan@gmail.com>
2020-06-10 16:36:06 -0700

    Update packages and resolve incompatibility

--- src/figures/figureW.jl
+++ src/figures/figureW.jl
@@ -7,7 +7,7 @@ function plotActualvFit(odf, dataType, colorL::Symbol, shapeL::Symbol)
         color = colorL,
         shape = shapeL,
         Guide.colorkey(),
-        Guide.shapekey(pos = [0.05w, -0.2h]),
+        Guide.shapekey(),
         Scale.y_continuous(minvalue = 0.0, maxvalue = 1.0),
         Geom.abline(color = "red"),
         Guide.xlabel("Actual effect"),
@@ -28,7 +28,7 @@ function plotActualvPredict(odf, dataType, colorL::Symbol, shapeL::Symbol)
         color = colorL,
         shape = shapeL,
         Guide.colorkey(),
-        Guide.shapekey(pos = [0.05w, -0.3h]),
+        Guide.shapekey(),
         Geom.abline(color = "red"),
         Guide.xlabel("Actual effect"),
         Guide.ylabel("LOO predicted effect"),
@@ -88,10 +88,10 @@ function plotDepletionSynergy(IgGXidx::Int64, IgGYidx::Int64, fit::fitResult; L0
         X1 = vcat(X1, Kav_df[!, :C1q]' * IgGC)
         X2 = vcat(X2, Kav_df[!, :C1q]' * IgGC)
     end
-
     @assert size(X, 1) == length(fit.x)
     @assert size(X1, 1) == length(fit.x)
     @assert size(X2, 1) == length(fit.x)
+
     output = exponential(Matrix(X'), fit)
     D1 = exponential(Matrix(X1'), fit)
     D2 = reverse(exponential(Matrix(X2'), fit))
@@ -214,7 +214,7 @@ function figureW(dataType, intercept = false, preset = false; L0 = 1e-9, f = 4, 
     end
 
     fit, odf, wdf = CVResults(df, intercept, preset_W; L0 = L0, f = f, murine = murine)
-    @assert all(in(names(odf)).([color, shape]))
+    @assert all(in(propertynames(odf)).([color, shape]))
     p1 = plotActualvFit(odf, dataType, color, shape)
     p2 = plotActualvPredict(odf, dataType, color, shape)
     p3 = plotCellTypeEffects(wdf, dataType)
