de2d0daf87a03752912f87b229f6889e997464b0
ctan <cyztan@gmail.com>
2020-07-29 19:21:48 -0700

    Figure 2 change, figure 3 as single file

--- src/figures/figure2.jl
+++ src/figures/figure2.jl
@@ -1,29 +1,25 @@
 function figure2()
     setGadflyTheme()
     fig_melanoma = figureW("melanoma"; IgGX = 2, IgGY = 3, L0 = 1e-9, f = 4, murine = true, legend = false)
-    fig_ITP = figureW("ITP"; IgGX = 1, IgGY = 2, L0 = 1e-8, f = 10, murine = true, legend = false)
+    fig_ITP = figureW("ITP"; IgGX = 1, IgGY = 2, L0 = 1e-8, f = 10, murine = true, legend = true)
     fig_blood = figureW("blood"; IgGX = 1, IgGY = 2, L0 = 1e-10, f = 4, murine = true, legend = false)
     fig_bone = figureW("bone"; IgGX = 1, IgGY = 2, L0 = 1e-10, f = 4, murine = true)
     fig_HIV = figureW("HIV"; IgGX = 1, IgGY = 2, L0 = 1e-8, f = 24, murine = true)
 
     draw(
-        SVG("figure2.svg", 20inch, 7inch),
+        SVG("figure2.svg", 7inch, 7inch),
         plotGrid(
-            (2, 5),
-            [fig_melanoma[1] fig_ITP[1] fig_blood[1] fig_bone[1] fig_HIV[1] fig_melanoma[2] fig_ITP[2] fig_blood[2] fig_bone[2] fig_HIV[2]];
-            widths = [4 4 4 5 5; 5 5 5 6 6],
-            heights = [],
+            (2, 2),
+            [fig_melanoma[1] fig_ITP[1] fig_melanoma[3] fig_ITP[3]];
+            widths = [3 4; 1 1],
         ),
     )
-end
 
-function figure3()
-    setGadflyTheme()
-    fig_melanoma = figureW("melanoma"; IgGX = 2, IgGY = 3, L0 = 1e-9, f = 4, murine = true)
-    fig_ITP = figureW("ITP"; IgGX = 1, IgGY = 2, L0 = 1e-8, f = 10, murine = true)
-    fig_blood = figureW("blood"; IgGX = 1, IgGY = 2, L0 = 1e-10, f = 4, murine = true)
-    fig_bone = figureW("bone"; IgGX = 1, IgGY = 2, L0 = 1e-10, f = 4, murine = true)
-    fig_HIV = figureW("HIV"; IgGX = 1, IgGY = 2, L0 = 1e-8, f = 24, murine = true)
-
-    draw(SVG("figure3.svg", 15inch, 4inch), plotGrid((1, 5), [fig_melanoma[3] fig_ITP[3] fig_blood[3] fig_bone[3] fig_HIV[3]]))
+    draw(
+        SVG("figureS2.svg", 16inch, 7inch),
+        plotGrid(
+            (2, 4),
+            [fig_melanoma[2] fig_ITP[2] fig_blood[1] fig_blood[2] fig_bone[1] fig_bone[2] fig_HIV[1] fig_HIV[2]];
+        ),
+    )
 end
