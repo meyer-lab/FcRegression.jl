2eb8e48a29f22839de65071f793e8643da95b8c0
ctan <cyztan@gmail.com>
2020-09-01 21:04:22 -0700

    Receptor abundance plot

--- src/FcRegression.jl
+++ src/FcRegression.jl
@@ -18,6 +18,7 @@ include("figures/figure2.jl")
 include("figures/figure3.jl")
 include("figures/figureB1.jl")
 include("figures/figureS.jl")
+include("figures/figureRR1.jl")
 include("figures/figureDiseaseSpecific.jl")
 include("figures/figureCelltypeSynergy.jl")
 
@@ -51,6 +52,7 @@ function figureAll()
     figure_HSyn_EO()
 
     figure_fitActI()
+    figureRR1()
 end
 
 export polyfc, polyfc_ActV
