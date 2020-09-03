4bb07a16127dba2c3de266e0ed633d6cac419e34
ctan <cyztan@gmail.com>
2020-09-02 19:09:28 -0700

    The fourth ActI trial

--- src/fitActI.jl
+++ src/fitActI.jl
@@ -1,6 +1,6 @@
 using Optim
 
-function fitActI(dataType; L0 = 1e-10, f = 4, murine::Bool = true, lower::Array{Float64} = nothing, upper::Array{Float64} = nothing)
+function fitActI(dataType; L0 = 1e-10, f = 4, murine::Bool = true, lower = nothing, upper = nothing, init = nothing)
     df = murine ? importDepletion(dataType) : importHumanized(dataType)
     func = ActI -> fitRegression(df; L0 = L0, f = f, murine = murine, ActI = ActI).r
     if lower == nothing && upper == nothing
@@ -12,7 +12,10 @@ function fitActI(dataType; L0 = 1e-10, f = 4, murine::Bool = true, lower::Array{
             upper = [10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0]
         end
     end
-    opt = optimize(func, lower, upper, Float64.(murine ? murineActI : humanActI))
+    if init == nothing
+        init = Float64.(murine ? murineActI : humanActI)
+    end
+    opt = optimize(func, lower, upper, init)
     if murine
         return figureW(dataType; L0 = L0, f = f, murine = true, IgGX = 3, IgGY = 5, ActI = opt.minimizer), opt.minimizer
     else
@@ -43,6 +46,12 @@ function figure_fitActI()
         p1, p2, p3, p4, p5, p6 = pls
         draw(
             SVG("figure_fitActI3_M" * dataType * ".svg", 1200px, 800px),
+            plotGrid((2, 3), [p1, p2, p3, p4, p5, p6], join(string.(w), " "); widths = [5 5 4; 4 6 4]),
+        )
+        pls, w = fitActI(dataType; L0 = L0s[i], f = fs[i], murine = true, lower = [0.0, 0, 0, 0], upper = [5.0, 5, 5, 5], init = [1.0, 1, 1, 1])
+        p1, p2, p3, p4, p5, p6 = pls
+        draw(
+            SVG("figure_fitActI4_M" * dataType * ".svg", 1200px, 800px),
             plotGrid((2, 3), [p1, p2, p3, p4, p5, p6], join(string.(w), " "); widths = [5 5 4; 4 6 4]),
         )
     end
