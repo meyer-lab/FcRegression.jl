e3c3447d1ce7963e06e091ab51156ca3ff1ae3eb
ctan <cyztan@gmail.com>
2020-10-21 12:19:12 -0700

    Logscale on mixture

--- src/mixture.jl
+++ src/mixture.jl
@@ -51,7 +51,7 @@ function TwoDFit(X::Matrix, Y::Matrix)
 end
 
 
-function MixtureFitLoss(df, ValConv::Vector, ExpConv::Vector)
+function MixtureFitLoss(df, ValConv::Vector, ExpConv::Vector; logscale = false)
     df = copy(df)
     df[!, "Predict"] .= 0.0
     for i = 1:size(df)[1]
@@ -59,32 +59,44 @@ function MixtureFitLoss(df, ValConv::Vector, ExpConv::Vector)
     end
     dtype = promote_type(eltype(df."Value"), eltype(ValConv), eltype(ExpConv))
     df[!, "Adjusted"] .= convert.(dtype, df[!, "Value"])
+    df[df[!, "Adjusted"] .<= 1.0, "Adjusted"] .= 1.0
+
+    @assert all(df[!, "Adjusted"] .>= 1.0)
+    @assert all(df[!, "Predict"] .>= 1.0)
+    if any(ValConv .<= 0.0) || any(ExpConv .<= 0.0)
+        return Inf, df
+    end
+
     loss = 0.0
     for (iv, val) in enumerate(unique(df."Valency"))
         for (ie, exp) in enumerate(unique(df."Experiment"))
             sdf = @view df[(df."Valency" .== val) .& (df."Experiment" .== exp), :]
             sdf."Adjusted" .*= ValConv[iv] * ExpConv[ie]
-            loss += norm(sdf."Adjusted" .- sdf."Predict", 2) / nrow(sdf)
+            if logscale
+                loss += norm(log.(sdf."Adjusted") .- log.(sdf."Predict"), 2) / nrow(sdf)
+            else
+                loss += norm(sdf."Adjusted" .- sdf."Predict", 2) / nrow(sdf)
+            end
         end
     end
     return loss, df
 end
 
 
-function MixtureFit(df)
+function MixtureFit(df; logscale = false)
     nv, ne = length(unique(df."Valency")), length(unique(df."Experiment"))
-    f(p::Vector, q::Vector) = MixtureFitLoss(df, [1.0; p], q)[1]
+    f(p::Vector, q::Vector) = MixtureFitLoss(df, [1.0; p], q; logscale = logscale)[1]
     f(v::Vector) = f(v[1:(nv - 1)], v[nv:end])
     init_v = ones(nv + ne - 1)
     od = OnceDifferentiable(f, init_v; autodiff = :forward)
     res = optimize(od, init_v, BFGS()).minimizer
     p, q = [1.0; res[1:(nv - 1)]], res[nv:end]
-    res = MixtureFitLoss(df, p, q)
+    res = MixtureFitLoss(df, p, q; logscale = logscale)
     return Dict("loss" => res[1], "df" => res[2], "ValConv" => p, "ExpConv" => q)
 end
 
 
-function plotValLoss(df, title = "")
+function plotValLoss(df, title = ""; logscale = false)
     df = copy(df)
     df[!, "NewValency"] .= 0
 
@@ -93,7 +105,7 @@ function plotValLoss(df, title = "")
         df[(df."Valency" .== 4), "NewValency"] .= val1
         for val2 = 1:40
             df[(df."Valency" .== 33), "NewValency"] .= val2
-            losses[val1, val2] = log(MixtureFit(df)["loss"])
+            losses[val1, val2] = log(MixtureFit(df; logscale = logscale)["loss"])
         end
     end
     pl = spy(losses, Guide.xlabel("Fitted valency for f=33"), Guide.ylabel("Fitted valency for f=4"), Guide.title("Log loss for " * title))
@@ -109,7 +121,7 @@ end
 
 
 """ Use for only one pair of IgG subclasses and cell line / Fc receptor"""
-function plotMixContinuous(df)
+function plotMixContinuous(df; logscale = false)
     df = copy(df)
     @assert length(unique(df."Cell")) == 1
     @assert length(unique(df."subclass_1")) == 1
@@ -130,6 +142,7 @@ function plotMixContinuous(df)
         layer(x = x, y = preds33, Geom.line, Theme(default_color = colorant"green", line_width = 2px)),
         layer(df, x = "%_1", y = "Adjusted", color = "Experiment", shape = "Valency"),
         Scale.x_continuous(labels = n -> "$IgGXname $(n*100)%\n$IgGYname $(100-n*100)%"),
+        (logscale ? Scale.y_log10 : Scale.y_continuous),
         Guide.xlabel(""),
         Guide.ylabel("Lbound", orientation = :vertical),
         Guide.manual_color_key("Predictions", ["f = 4", "f = 33"], ["red", "green"]),
@@ -142,17 +155,27 @@ end
 
 function plotMixtures()
     setGadflyTheme()
-    df = MixtureFit(loadMixData())["df"]
-    draw(SVG("figure_mixture_valencies.svg", 700px, 300px), plotGrid((1, 1), [plotValLoss(df, "all data")]))
-    cells = unique(df."Cell")
-    pairs = unique([r."subclass_1" * "-" * r."subclass_2" for r in eachrow(df)])
+    res1 = MixtureFit(loadMixData(); logscale = false)
+    println("Linear Scale: Fitted ValConv = ", res1["ValConv"], ", ExpConv = ", res1["ExpConv"])
+    df1 = res1["df"]
+    res2 = MixtureFit(loadMixData(); logscale = true)
+    println("Log Scale: Fitted ValConv = ", res2["ValConv"], ", ExpConv = ", res2["ExpConv"])
+    df2 = res2["df"]
+    #draw(SVG("figure_mixture_valencies_linear.svg", 700px, 300px), plotGrid((1, 1), [plotValLoss(df, "all data"; logscale = false)]))
+    #draw(SVG("figure_mixture_valencies_log.svg", 700px, 300px), plotGrid((1, 1), [plotValLoss(df, "all data"; logscale = true)]))
+    cells = unique(df1."Cell")
+    pairs = unique([r."subclass_1" * "-" * r."subclass_2" for r in eachrow(df1)])
 
-    pls = Matrix(undef, length(cells), length(pairs))
+    pls1 = Matrix(undef, length(cells), length(pairs))
+    pls2 = Matrix(undef, length(cells), length(pairs))
     for (i, pair) in enumerate(pairs)
         for (j, cell) in enumerate(cells)
-            ndf = df[(df."Cell" .== cell) .& (df."subclass_1" .== split(pair, "-")[1]) .& (df."subclass_2" .== split(pair, "-")[2]), :]
-            pls[j, i] = plotMixContinuous(ndf)
+            ndf1 = df1[(df1."Cell" .== cell) .& (df1."subclass_1" .== split(pair, "-")[1]) .& (df1."subclass_2" .== split(pair, "-")[2]), :]
+            pls1[j, i] = plotMixContinuous(ndf1; logscale = false)
+            ndf2 = df2[(df2."Cell" .== cell) .& (df2."subclass_1" .== split(pair, "-")[1]) .& (df2."subclass_2" .== split(pair, "-")[2]), :]
+            pls2[j, i] = plotMixContinuous(ndf2; logscale = true)
         end
     end
-    draw(SVG("figure_mixture_continuous.svg", 2500px, 1000px), plotGrid(size(pls), pls))
+    draw(SVG("figure_mixture_continuous_linear.svg", 2500px, 1000px), plotGrid(size(pls1), pls1))
+    draw(SVG("figure_mixture_continuous_log.svg", 2500px, 1000px), plotGrid(size(pls2), pls2))
 end
