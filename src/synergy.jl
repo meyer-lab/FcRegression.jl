c30290e88142b9a31cc1f5869a7ac068aa0096bf
Stephen Lees <stephendl102@gmail.com>
2020-11-05 16:20:23 -0800

    Add synergy options

--- src/synergy.jl
+++ src/synergy.jl
@@ -6,7 +6,7 @@ function calcSynergy(
     f,
     FcExpr = nothing;
     murine,
-    fit::Union{optResult, Nothing},
+    fit::Union{optResult, Nothing} = nothing,
     Rbound = false,
     c1q = false,
     neutralization = false,
@@ -39,18 +39,36 @@ function calcSynergy(
             fit = optResult(fit.cellWs, fit.ActI, fit.residual)
             fit.cellWs = fit.cellWs[1:(end - 1)]
         end
+        if ndims(FcExpr) == 1
+            if !Rbound #Activity of single cell or receptor with disease type
+                ActI = fit.ActI
+                print(ActI)
+                D1 = dropdims(D1, dims = 1)
+                D2 = dropdims(D2, dims = 1)
+                combine = dropdims(combine, dims = 1)
+                D1 = D1' * ActI
+                D2 = (D2' * ActI)
+                combine = combine' * ActI
+            end
+            D2 = reverse(D2) #Binding only
+            D1[D1 .<= 0.0] .= 0.0
+            D2[D2 .<= 0.0] .= 0.0
+            additive = D1 + D2
+            combine[combine .<= 0.0] .= 0.0
+            additive[additive .<= 0.0] .= 0.0
+        else #Depletion
+            @assert size(combine, 1) + (c1q ? 1 : 0) == length(fit.cellWs)
+            @assert size(D1, 1) + (c1q ? 1 : 0) == length(fit.cellWs)
+            @assert size(D2, 1) + (c1q ? 1 : 0) == length(fit.cellWs)
 
-        @assert size(combine, 1) + (c1q ? 1 : 0) == length(fit.cellWs)
-        @assert size(D1, 1) + (c1q ? 1 : 0) == length(fit.cellWs)
-        @assert size(D2, 1) + (c1q ? 1 : 0) == length(fit.cellWs)
-
-        additive = exponential(regressionPred(D1 + reverse(D2; dims = 3), (c1q ? D1df .+ D2df[nPoints:-1:1, :] : nothing), fit))
-        combine = exponential(regressionPred(combine, combinedf, fit))
-        D1 = exponential(regressionPred(D1, D1df, fit))
-        D2 = reverse(exponential(regressionPred(D2, D2df, fit)))
-    else  # single cell activity
+            additive = exponential(regressionPred(D1 + reverse(D2; dims = 3), (c1q ? D1df .+ D2df[nPoints:-1:1, :] : nothing), fit))
+            combine = exponential(regressionPred(combine, combinedf, fit))
+            D1 = exponential(regressionPred(D1, D1df, fit))
+            D2 = reverse(exponential(regressionPred(D2, D2df, fit)))
+        end
+    else 
         @assert ndims(FcExpr) == 1
-        if !Rbound #Activity of single cell type
+        if !Rbound #Activity of single cell without disease type
             ActI = murine ? murineActI : humanActI
             D1 = dropdims(D1, dims = 1)
             D2 = dropdims(D2, dims = 1)
@@ -135,55 +153,58 @@ function plotDepletionSynergy(
             IgGXidx, IgGYidx = 1, 2
         end
     end
+
     if Cellidx == nothing && Recepidx != nothing
         @error "Must specify Cellidx AND Recepidx"
     end
+    if dataType != nothing && fit == nothing
+        @error "Fit must be provided with dataType"
+    end
     @assert IgGXidx != IgGYidx
+
     Xname = murine ? murineIgGFucose[IgGXidx] : humanIgG[IgGXidx]
     Yname = murine ? murineIgGFucose[IgGYidx] : humanIgG[IgGYidx]
     Kav_df = importKav(; murine = murine, IgG2bFucose = murine, c1q = c1q, retdf = true)
     Kav = Matrix{Float64}(Kav_df[!, murine ? murineFcgR : humanFcgR])
     Receps = murine ? murineFcgR : humanFcgR
     nPoints = 100
+    ymax = nothing
+    
+    if Recepidx != nothing # look at only one receptor
+        FcExpr = zeros(length(Receps))
+        FcExpr[Recepidx] = importRtot(murine = murine)[Recepidx, Cellidx]
+        ylabel = "Activity"
+    elseif Cellidx != nothing # look at only one cell FcExpr
+        FcExpr = importRtot(murine = murine)[:, Cellidx]
+        ylabel = "Activity"
+    else
+        FcExpr = importRtot(; murine = murine)
+    end
 
     if fit != nothing  # use disease model
         if Recepidx != nothing # look at only one receptor
-            FcExpr = zeros(length(Receps))
-            FcExpr[Recepidx] = importRtot(murine = murine)[Recepidx, Cellidx]
             title = "$(murine ? murineFcgR[Recepidx] : humanFcgR[Recepidx]) $dataType"
         elseif Cellidx != nothing # look at only one cell FcExpr
-            FcExpr = importRtot(murine = murine)[:, Cellidx]
             title = "$(cellTypes[Cellidx]) $dataType"
         else
-            FcExpr = importRtot(; murine = murine)
+            ylabel = "Depletion"
             title = "$dataType"
+            ymax = 1.0
         end
-        ylabel = "Depletion"
-        ymax = 1.0
-        D1, D2, additive, output = calcSynergy(IgGXidx, IgGYidx, L0, f, FcExpr; murine = murine, fit = fit, c1q = c1q, neutralization = neutralization)
-    elseif Cellidx != nothing 
-        if murine
-            ymax = murineActYmax[Cellidx]
-        else
-            ymax = humanActYmax[Cellidx]
-        end
-        if Recepidx == nothing  # single cell type
-            FcExpr = importRtot(murine = murine)[:, Cellidx]
-            ylabel = "Activity"
-            title = "$(cellTypes[Cellidx])"
-            D1, D2, additive, output =
-                calcSynergy(IgGXidx, IgGYidx, L0, f, FcExpr; murine = murine, fit = fit, Rbound = Rbound, nPoints = nPoints)
-        else  # bind to one receptor
-            FcExpr = zeros(length(Receps))
-            FcExpr[Recepidx] = importRtot(murine = murine)[Recepidx, Cellidx]
-            D1, D2, additive, output =
-                calcSynergy(IgGXidx, IgGYidx, L0, f, FcExpr; murine = murine, fit = fit, Rbound = Rbound, nPoints = nPoints)
-            if Rbound
-            ylabel = "$(murine ? murineFcgR[Recepidx] : humanFcgR[Recepidx]) Binding"
-            title = "$(murine ? murineFcgR[Recepidx] : humanFcgR[Recepidx])"
-        end
+    elseif Recepidx != nothing  # bind to one receptor
+        title = "$(murine ? murineFcgR[Recepidx] : humanFcgR[Recepidx]), $(cellTypes[Cellidx])"
+    elseif Cellidx != nothing  # bind to one cell type
+        title = "$(cellTypes[Cellidx])"
     else
         @error "Not allowed combination of fit/Cellidx/Recepidx."
+    end
+
+    D1, D2, additive, output = calcSynergy(IgGXidx, IgGYidx, L0, f, FcExpr; murine = murine, fit = fit, Rbound = Rbound, c1q = c1q, neutralization = neutralization)
+    if Rbound
+        ylabel = "Binding"
+    end
+    if ymax == nothing
+        ymax = 1.1*maximum(additive)
     end
 
     x = range(0.0, 1.0; length = nPoints)
@@ -203,7 +224,7 @@ function plotDepletionSynergy(
         Guide.ylabel("Predicted $ylabel", orientation = :vertical),
         Coord.cartesian(ymin = 0, ymax = ymax),
         Guide.manual_color_key("", ["Predicted", "Additive", "$Xname only", "$Yname only"], ["green", "red", "blue", "orange"]),
-        Guide.title("Predicted effects vs $Xname-$Yname Composition ($title)"),
+        Guide.title("$Xname-$Yname Composition ($title)"),
         style(key_position = :inside),
     )
     return pl
