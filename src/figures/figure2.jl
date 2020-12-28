424bf2e6981bdcf3d2122edc7aea2133aea2fb11
ctan <cyztan@gmail.com>
2020-12-28 14:17:56 -0800

    Add line in Figure 2

--- src/figures/figure2.jl
+++ src/figures/figure2.jl
@@ -12,11 +12,13 @@ function figure2()
         y = "Predict",
         color = "Valency",
         shape = "Cell",
+        Geom.point,
         Guide.xlabel("Actual"),
         Guide.ylabel("Predicted", orientation = :vertical),
         Scale.x_log10,
         Scale.y_log10,
         Scale.color_discrete_manual(Scale.color_discrete().f(3)[1], Scale.color_discrete().f(3)[3]),
+        Geom.abline(color="green"),
     )
 
     draw(SVG("figure2.svg", 1300px, 600px), plotGrid((1, 2), [nothing, pl]))
