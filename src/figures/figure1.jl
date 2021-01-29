aea6539a28907020ae75da452a322c1fd1f89c15
aarmey <aarmey@users.noreply.github.com>
2021-01-29 18:21:29 +0000

    Format .jl files

--- src/figures/figure1.jl
+++ src/figures/figure1.jl
@@ -3,7 +3,7 @@
 function figure1()
     setGadflyTheme()
     draw(SVG("figure1.svg", 2500px, 1000px), plotMixOriginalData(loadMixData()))
-    draw(SVG("figure1A.svg", 2500px, 1000px), plotMixOriginalData(PCAData(; cutoff=0.95)))
-    draw(SVG("figure1B.svg", 2500px, 1000px), plotMixOriginalData(PCAData(; cutoff=0.9)))
-    draw(SVG("figure1C.svg", 2500px, 1000px), plotMixOriginalData(PCAData(; cutoff=0.8)))
+    draw(SVG("figure1A.svg", 2500px, 1000px), plotMixOriginalData(PCAData(; cutoff = 0.95)))
+    draw(SVG("figure1B.svg", 2500px, 1000px), plotMixOriginalData(PCAData(; cutoff = 0.9)))
+    draw(SVG("figure1C.svg", 2500px, 1000px), plotMixOriginalData(PCAData(; cutoff = 0.8)))
 end
