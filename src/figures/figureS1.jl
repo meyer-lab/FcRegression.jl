347f41546b08ae86bccdb05f067b39a4b5c4c5f2
Stephen Lees <stephendl102@gmail.com>
2021-02-02 17:11:44 -0800

    Use theoretical EC50 instead of actual

--- src/figures/figureS1.jl
+++ src/figures/figureS1.jl
@@ -9,8 +9,12 @@ function figureS1()
     L0 = 1e-9
     f = 4
 
-    res, odf, Cell_df, ActI_df = regressionResult(dataType; L0 = L0, f = f, murine = true)
+    res, odf, Cell_df, ActI_df = regressionResult(dataType; L0 = L0, f = f)
 
-    p1 = mixEC50()
-    draw(SVG("figureS1.svg", 10inch, 8inch), plotGrid((1, 1), [p1]; widths = [3]))
+    p1 = plotEC50(L0, f; dataType = dataType, fit = res, Cellidx = 6, Recepidx = 2, Rbound = true)
+    #p1 = mixEC50()
+    draw(
+        SVG("figureS1.svg", 10inch, 8inch),
+        plotGrid((1,1), [p1]; widths = [3]),
+    )
 end
