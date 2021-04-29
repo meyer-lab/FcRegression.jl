0b48cc186e75adb95ec33c09ba4c8d6dce2f06ac
ctan <cyztan@gmail.com>
2021-04-28 22:00:11 -0700

    Using PC1 of measured data as measurement

--- src/figures/figure2.jl
+++ src/figures/figure2.jl
@@ -1,26 +1,29 @@
 """ Figure 2: we can accurately account for mixed ICs """
 
-function figure2()
+function plotPredvsMeasured(df, xx = "Adjusted", yy = "Predict", xxlabel = "Actual", yylabel = "Predicted")
     setGadflyTheme()
-
-    df = MixtureCellSeparateFit(loadMixData(); logscale = true)
-    df[!, "%_1"] ./= 100.0
     df[!, "Valency"] .= Symbol.(df[!, "Valency"])
+    df[(df[!, xx]) .< 1.0, xx] .= 1.0
+    df[(df[!, yy]) .< 1.0, yy] .= 1.0
 
-    pl = plot(
+    return plot(
         df,
-        x = "Adjusted",
-        y = "Predict",
+        x = xx,
+        y = yy,
         color = "Valency",
         shape = "Cell",
         Geom.point,
-        Guide.xlabel("Actual"),
-        Guide.ylabel("Predicted", orientation = :vertical),
+        Guide.xlabel(xxlabel),
+        Guide.ylabel(yylabel, orientation = :vertical),
         Scale.x_log10,
         Scale.y_log10,
         Scale.color_discrete_manual(Scale.color_discrete().f(3)[1], Scale.color_discrete().f(3)[3]),
         Geom.abline(color = "green"),
     )
+end
 
-    draw(SVG("figure2.svg", 1300px, 600px), plotGrid((1, 2), [nothing, pl]))
+
+function figure2()
+    df = MixtureCellSeparateFit(loadMixData(); logscale = true)
+    draw(SVG("figure2.svg", 1300px, 600px), plotGrid((1, 2), [nothing, plotPredvsMeasured(df)]))
 end
