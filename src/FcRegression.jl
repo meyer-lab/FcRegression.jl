97a5e17e12c7f068fdbd3de2f337895e58b20772
Aaron Meyer <ameyer@ucla.edu>
2021-08-11 11:59:39 -0700

    Remove unused code

--- src/FcRegression.jl
+++ src/FcRegression.jl
@@ -4,7 +4,6 @@ import Distances
 using polyBindingModel
 using Optim
 using DataFrames
-using Dierckx
 
 include("figures/figureCommon.jl")
 
@@ -13,7 +12,6 @@ include("dataHelpers.jl")
 include("mixture.jl")
 include("regression.jl")
 include("synergy.jl")
-include("ExtraFunctions.jl")
 
 include("figures/figureW.jl")
 
@@ -22,7 +20,6 @@ include("figures/figure2.jl")
 include("robinett.jl")
 include("figures/figure3.jl")
 include("figures/figure4.jl")
-include("figures/figureS1.jl")
 include("figures/figureS2.jl")
 
 include("figures/extra/figureS.jl")
@@ -41,7 +38,6 @@ function figureAll()
     figure3()
     figure4()
 
-    figureS1()
     figureS2()
 end
 
