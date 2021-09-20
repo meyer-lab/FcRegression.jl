afdcaa4605f8f05ab909b614bb29e92505cee29b
Cyrillus Tan <cyztan@gmail.com>
2021-09-19 22:43:18 -0700

    Small figure changes (#415)
    * Small figure changes
    
    * Fix figure issues

--- src/figures/figure1.jl
+++ src/figures/figure1.jl
@@ -55,27 +55,21 @@ function bindVSaff()
     )
     df."Affinity" = [hKav[hKav."IgG" .== r."Subclass", r."Cell"][1] for r in eachrow(df)]
     df[!, "Valency"] .= Symbol.(df[!, "Valency"])
-    slope = ols(df."Affinity", df."Value")
-    ols_x = 10 .^ (0:1:ceil(log10(maximum(df."xmax"))))
-    ols_y = ols_x .* slope
     pl1 = plot(
-        df,
-        x = "Value",
-        y = "Affinity",
-        xmin = "xmin",
-        xmax = "xmax",
+        df,   
+        x = "Affinity",
+        y = "Value",
+        ymin = "xmin",
+        ymax = "xmax",
         color = "Cell",
         shape = "Subclass",
         Geom.point,
         Geom.errorbar,
-        intercept = [148],
-        slope = [slope],
-        Geom.abline(color = "red", style = :dash),
         Scale.x_log10,
         Scale.y_log10,
-        Guide.title("Binding vs Affinity"),
-        Guide.xlabel("Binding quantification"),
-        Guide.ylabel("Affinity"),
+        Guide.title("Affinity vs Binding"),
+        Guide.xlabel("Affinity"),
+        Guide.ylabel("Binding quantification"),
     )
 
     val_ratio = combine(groupby(df, ["Cell", "Subclass"])) do df
@@ -107,7 +101,8 @@ function plot_PCA_score(df; title = "Score")
             append!(layers, layer(df[(df."Subclass Pair" .== pair) .& (df."Valency" .== val), :], x = "PC 1", y = "PC 2", color = [pair], Geom.line))
         end
     end
-    return plot(df, layers..., x = "PC 1", y = "PC 2", color = "Subclass Pair", Geom.point, Guide.title(title))
+    return plot(df, layers..., x = "PC 1", y = "PC 2", color = "Subclass Pair", Geom.point, Guide.title(title),
+        Guide.xticks(ticks=[-2e4, -1e4, 0, 1e4], orientation=:horizontal), Guide.yticks(ticks=[-5e3, 0, 5e3]))
 end
 
 function figure1()
@@ -116,20 +111,11 @@ function figure1()
     p1, p2 = bindVSaff()
 
     df = averageMixData()
-    _, _, vars_expl = mixtureDataPCA()
-    score_df4, loading_df4, vars_expl4 = mixtureDataPCA(; val = 4)
-    score_df33, loading_df33, vars_expl33 = mixtureDataPCA(; val = 33)
-    R2Xdat = vcat(
-        [
-            DataFrame(Components = 1:length(vars_expl), Data = i, R2X = j) for
-            (i, j) in [("Overall", vars_expl), ("4-valent IC", vars_expl4), ("33-valent IC", vars_expl33)]
-        ]...,
-    )
+    score, loading, vars_expl = mixtureDataPCA()
     vars = plot(
-        R2Xdat,
+        DataFrame(Components = 1:length(vars_expl), R2X = vars_expl),
         x = "Components",
         y = "R2X",
-        color = "Data",
         label = [@sprintf("%.2f %%", i * 100) for i in vars_expl],
         Geom.point,
         Geom.line,
@@ -141,21 +127,17 @@ function figure1()
         Guide.ylabel("R2X"),
     )
 
-    score_plot4 = plot_PCA_score(score_df4; title = "Score, 4-valent ICs")
-    loading_plot4 =
-        plot(loading_df4, x = "PC 1", y = "PC 2", color = "Cell", label = "Cell", Geom.point, Geom.label, Guide.title("Loading, 4-valent ICs"))
-    score_plot33 = plot_PCA_score(score_df33; title = "Score, 33-valent ICs")
-    loading_plot33 =
-        plot(loading_df33, x = "PC 1", y = "PC 2", color = "Cell", label = "Cell", Geom.point, Geom.label, Guide.title("Loading, 33-valent ICs"))
-
+    score_plot4 = plot_PCA_score(score[score."Valency" .== 4, :]; title = "Score, 4-valent ICs")
+    score_plot33 = plot_PCA_score(score[score."Valency" .== 33, :]; title = "Score, 33-valent ICs")
+    loading_plot = plot(loading, x = "PC 1", y = "PC 2", color = "Cell", label = "Cell", Geom.point, Geom.label, Guide.title("Loading"))
 
     # Specific IgG pair - receptor interaction
     igg12_1 = splot_origData(df[(df."Cell" .== "FcgRI") .& (df."subclass_1" .== "IgG1") .& (df."subclass_2" .== "IgG2"), :]; match_y = false)
     igg14_1 = splot_origData(df[(df."Cell" .== "FcgRIIIA-158F") .& (df."subclass_1" .== "IgG1") .& (df."subclass_2" .== "IgG4"), :]; match_y = false)
     pl = plotGrid(
         (3, 4),
-        [nothing, p1, p2, nothing, nothing, vars, igg12_1, igg14_1, score_plot4, loading_plot4, score_plot33, loading_plot33];
-        sublabels = [1 1 1 1 0 1 1 1 1 1 1 1],
+        [nothing, p1, p2, nothing, nothing, vars, igg12_1, igg14_1, score_plot4, score_plot33, loading_plot, nothing];
+        sublabels = [1 1 1 1 0 1 1 1 1 1 1 0],
     )
     return draw(SVG("figure1.svg", 18inch, 12inch), pl)
 end
