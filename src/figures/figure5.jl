92651bb501fc2304935c089682e3df25c66ede7a
aarmey <aarmey@users.noreply.github.com>
2021-09-21 18:02:31 +0000

    Format .jl files

--- src/figures/figure5.jl
+++ src/figures/figure5.jl
@@ -5,9 +5,17 @@ function plot_mix_ITP_depletion()
     df[!, "Dose"] = [generate_mixture_name(dfr) for dfr in eachrow(df)]
     R2anno = "<i>R</i><sup>2</sup>" * @sprintf("=%.3f", R2(df."depletion", df."Predicted"; logscale = false))
     setGadflyTheme()
-    return plot(df, x="depletion", y="Predicted", color="Dose", Geom.point, Geom.abline(color = "green"), 
-        Guide.title("Mice <i>in vivo</i> platelet depletion"), Guide.xlabel("Measured"),
-        Guide.annotation(compose(context(), text(-0.6, 0.7, R2anno), fontsize(12pt), font("Helvetica"))))
+    return plot(
+        df,
+        x = "depletion",
+        y = "Predicted",
+        color = "Dose",
+        Geom.point,
+        Geom.abline(color = "green"),
+        Guide.title("Mice <i>in vivo</i> platelet depletion"),
+        Guide.xlabel("Measured"),
+        Guide.annotation(compose(context(), text(-0.6, 0.7, R2anno), fontsize(12pt), font("Helvetica"))),
+    )
 end
 
 function figure5()
