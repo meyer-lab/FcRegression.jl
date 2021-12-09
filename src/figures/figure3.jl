9409adfe7e7536d6084eade3aef2f275cdb82577
ctan <cyztan@gmail.com>
2021-12-09 01:45:22 -0800

    Remove unused S3 figure

--- src/figures/figure3.jl
+++ src/figures/figure3.jl
@@ -35,26 +35,10 @@ end
 
 function figure3()
     setGadflyTheme()
-    
+
     MplR2, Mpls, Msupp = compareRegMethodPlot("melanoma"; L0 = 1e-9, f = 6, murine = true)
     IplR2, Ipls, Isupp = compareRegMethodPlot("ITP"; L0 = 1e-8, f = 10, murine = true)
     
     draw(SVG("figure3.svg", 1300px, 600px), plotGrid((2, 4), [MplR2, Mpls..., IplR2, Ipls...]))
     draw(SVG("figureS3.svg", 900px, 1800px), plotGrid((6, 3), [Msupp..., Isupp...]))
-end
-
-function figureS3()
-    setGadflyTheme()
-
-    A1, C1, R1 = figureW("melanoma"; L0 = 1e-9, f = 6, murine = true, exp_method = false, fit_ActI = false)
-    A2, C2, R2 = figureW("melanoma"; L0 = 1e-9, f = 6, murine = true, exp_method = false, fit_ActI = true)
-    A3, C3, R3 = figureW("melanoma"; L0 = 1e-9, f = 6, murine = true, exp_method = true, fit_ActI = false)
-    A4, C4, R4 = figureW("melanoma"; L0 = 1e-9, f = 6, murine = true, exp_method = true, fit_ActI = true)
-    
-    A01, C01, R01 = figureW("ITP"; L0 = 1e-8, f = 10, murine = true, exp_method = false, fit_ActI = false)
-    A02, C02, R02 = figureW("ITP"; L0 = 1e-8, f = 10, murine = true, exp_method = false, fit_ActI = true)
-    A03, C03, R03 = figureW("ITP"; L0 = 1e-8, f = 10, murine = true, exp_method = true, fit_ActI = false)
-    A04, C04, R04 = figureW("ITP"; L0 = 1e-8, f = 10, murine = true, exp_method = true, fit_ActI = true)
-
-    draw(SVG("figureS3.svg", 900px, 2400px), plotGrid((8, 3), [A1,C1,R1,A2,C2,R2,A3,C3,R3,A4,C4,R4,A01,C01,R01,A02,C02,R02,A03,C03,R03,A04,C04,R04]))
 end
