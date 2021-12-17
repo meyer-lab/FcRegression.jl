e5b9ed9deecc9b7b80bb9ed0473b32b71c8017c7
aarmey <aarmey@users.noreply.github.com>
2021-12-17 18:02:35 +0000

    Format .jl files

--- src/figures/figure1.jl
+++ src/figures/figure1.jl
@@ -162,10 +162,30 @@ function figure1()
     SP33 = plot_PCA_score(score[score."Valency" .== 33, :]; title = "PCA Score, 33-valent ICs", xx = "PC 1", yy = "PC 2")
     SP4_13 = plot_PCA_score(score[score."Valency" .== 4, :]; title = "PCA Score, 4-valent ICs", xx = "PC 1", yy = "PC 3")
     SP33_13 = plot_PCA_score(score[score."Valency" .== 33, :]; title = "PCA Score, 33-valent ICs", xx = "PC 1", yy = "PC 3")
-    LP = plot(loading, x = "PC 1", y = "PC 2", color = "Cell", label = "Cell", Geom.point, Geom.label, Guide.title("PCA Loadings"),
-        Scale.x_continuous(minvalue=-1.0, maxvalue=1.0), Scale.y_continuous(minvalue=-1.0, maxvalue=1.0))
-    LP_13 = plot(loading, x = "PC 1", y = "PC 3", color = "Cell", label = "Cell", Geom.point, Geom.label, Guide.title("PCA Loadings"),
-        Scale.x_continuous(minvalue=-1.0, maxvalue=1.0), Scale.y_continuous(minvalue=-1.0, maxvalue=1.0))
+    LP = plot(
+        loading,
+        x = "PC 1",
+        y = "PC 2",
+        color = "Cell",
+        label = "Cell",
+        Geom.point,
+        Geom.label,
+        Guide.title("PCA Loadings"),
+        Scale.x_continuous(minvalue = -1.0, maxvalue = 1.0),
+        Scale.y_continuous(minvalue = -1.0, maxvalue = 1.0),
+    )
+    LP_13 = plot(
+        loading,
+        x = "PC 1",
+        y = "PC 3",
+        color = "Cell",
+        label = "Cell",
+        Geom.point,
+        Geom.label,
+        Guide.title("PCA Loadings"),
+        Scale.x_continuous(minvalue = -1.0, maxvalue = 1.0),
+        Scale.y_continuous(minvalue = -1.0, maxvalue = 1.0),
+    )
 
 
     pl = plotGrid((3, 5), [nothing, nothing, p1, p2, vars, SP4, SP33, LP, igg12_1, igg14_1, SP4_13, SP33_13, LP_13];)
