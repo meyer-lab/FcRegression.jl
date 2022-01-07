613cee4fc1046eaa35a022b839c937c1e798da41
ctan <cyztan@gmail.com>
2022-01-06 17:44:47 -0800

    Convert MCMC results to prediction df

--- src/mcmc.jl
+++ src/mcmc.jl
@@ -95,3 +95,20 @@ function plot_MCMC_dists(c = runMCMC())
     draw(SVG("MCMC_others.svg", 8inch, 4inch), other_plot)
 end
 
+
+function MCMCresults2fit(c = runMCMC())
+    m = DataFrame(mean(c))
+    m."order" .= 0
+    m[startswith.(String.(m."parameters"), "lRtot"), "order"] .= 1
+    m[startswith.(String.(m."parameters"), "lf"), "order"] .= 2
+    m[startswith.(String.(m."parameters"), "lKxStar"), "order"] .= 3
+    m[startswith.(String.(m."parameters"), "lKav"), "order"] .= 4
+    Rtot, vals, KxStar, Kav = dismantle_x0(exp.(sort(m, "order")."mean"))
+
+    return mixturePredictions(;
+        Rtot = Rtot,
+        Kav = Kav,
+        KxStar = KxStar,
+        vals = vals,
+    )
+end
