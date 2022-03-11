2806b1870c3ef3a9d354a081202d9c89f0f4715d
ctan <cyztan@gmail.com>
2022-03-10 19:25:21 -0800

    Change in Fig 4: first one should be binding to one receptor, so fix bug

--- src/figures/figure4.jl
+++ src/figures/figure4.jl
@@ -16,15 +16,16 @@ function figure4()
     Xdf = modelPred(df; L0 = L0, f = f, murine = true, cellTypes = nothing)
     res = fitRegNNLS(Xdf; murine = murine, cellTypes = nothing, exp_method = exp_method)
 
-    ITP_Dep = plotDepletionSynergy(1, 2; L0 = L0, f = f, dataType = "ITP", fit = res)
-    ITP_Dep_All = plotSynergy(L0, f; murine = true, dataType = dataType, fit = res)
+    ITP_Kupffer_Bound = plotDepletionSynergy(1, 2; L0 = L0, f = f, dataType = "ITP", fit = res, Cellidx = 6, Recepidx = 2,Rbound = true)
+    ITP_Kupffer_Bound_All = plotSynergy(L0, f; murine = true, dataType = dataType, fit = res, Cellidx = 6, Recepidx = 2, Rbound = true)
     ITP_Kupffer_Act = plotDepletionSynergy(1, 2; L0 = L0, f = f, dataType = "ITP", fit = res, Cellidx = 6)
     ITP_Kupffer_Act_All = plotSynergy(L0, f; murine = true, dataType = dataType, fit = res, Cellidx = 6)
-    ITP_Kupffer_Bound = plotDepletionSynergy(1, 2; L0 = L0, f = f, dataType = "ITP", fit = res, Cellidx = 6, Rbound = true)
-    ITP_Kupffer_Bound_All = plotSynergy(L0, f; murine = true, dataType = dataType, fit = res, Cellidx = 6, Rbound = true)
+    ITP_Dep = plotDepletionSynergy(1, 2; L0 = L0, f = f, dataType = "ITP", fit = res)
+    ITP_Dep_All = plotSynergy(L0, f; murine = true, dataType = dataType, fit = res)
+    
 
     draw(
         SVG("figure4.svg", 10inch, 8inch),
-        plotGrid((2, 3), [ITP_Dep ITP_Kupffer_Act ITP_Kupffer_Bound ITP_Dep_All ITP_Kupffer_Act_All ITP_Kupffer_Bound_All]),
+        plotGrid((2, 3), [ITP_Kupffer_Bound ITP_Kupffer_Act ITP_Dep ITP_Kupffer_Bound_All ITP_Kupffer_Act_All ITP_Dep_All]),
     )
 end
