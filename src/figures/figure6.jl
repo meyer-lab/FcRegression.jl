261e93e45b573b9b81008ca7a46e19a20c049579
Cyrillus <cyztan@gmail.com>
2022-04-19 14:50:42 -0700

    Naming change for figures (add murine as Fig 3)

---
+++ src/figures/figure6.jl
@@ -0,0 +1,26 @@
+""" Figure 6: validating the in vivo effect of mixtures """
+
+function plot_mix_ITP_depletion()
+    df = invivo_prediction(importDeplExp(); L0 = 1e-9, f = 4, KxStar = KxConst)
+    df[!, "Dose"] = [generate_mixture_name(dfr) for dfr in eachrow(df)]
+    R2anno = "<i>R</i><sup>2</sup>" * @sprintf("=%.3f", R2(df."depletion", df."Predicted"; logscale = false))
+    setGadflyTheme()
+    return plot(
+        df,
+        x = "depletion",
+        y = "Predicted",
+        color = "Dose",
+        Geom.point,
+        Geom.abline(color = "green"),
+        Guide.title("Mice <i>in vivo</i> platelet depletion"),
+        Guide.xlabel("Measured"),
+        Guide.annotation(compose(context(), text(-0.6, 0.7, R2anno), fontsize(12pt), font("Helvetica"))),
+    )
+end
+
+function figure6()
+    setGadflyTheme()
+    pl = plot_mix_ITP_depletion()
+    draw(SVG("figure6.svg", 6inch, 4inch), pl)
+    draw(PDF("figure6.pdf", 6inch, 4inch), pl)
+end
