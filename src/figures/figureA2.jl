7b3339e3dde73b82042f162aded902186297ecae
Cyrillus <cyztan@gmail.com>
2022-07-01 15:53:35 -0700

    Reset figure file names

---
+++ src/figures/figureA2.jl
@@ -0,0 +1,40 @@
+""" Figure 2: we can accurately account for mixed ICs """
+
+function figure2()
+    raw_predict = predMix(
+        averageMixData(loadMixData());
+        Kav = importKav(; murine = false, retdf = true),
+        Rtot = importRtotDist(:hCHO; regular = true, retdf = true),
+    )
+
+    raw_pred_pl = plotPredvsMeasured(
+        raw_predict;
+        xx = "Value",
+        xxlabel = "Measured",
+        color = "Receptor",
+        shape = "Valency",
+        title = "Raw model predictions without fitting",
+        R2pos = (0, -2.7),
+    )
+
+    df = loadMixData()
+    Kav_old = importKavDist(; murine = false, regularKav = true, retdf = true)
+    c_noKav = rungMCMC("humanNUTSfit_0517_KavOnly_v1.8.dat"; dat = :hCHO, Kavd = Kav_old)
+    pl_noKav = plotMCMCPredict(c_noKav, df; dat = :hCHO, Kav = Kav_old, R2pos = (0, -2), title = "Predictions with all but affinity fitting")
+
+    c = rungMCMC("humanNUTSfit_0505.dat"; dat = :hCHO, mcmc_iter = 1_000)
+
+    pl1 = plotMCMCPredict(c, df; dat = :hCHO, title = "All predictions with \nsingle hIgG fitted parameters", R2pos = (0, -2.5))
+    pl2 = plotMCMCPredict(
+        c,
+        df[(df."%_1" .!= 1.0) .& (df."%_2" .!= 1.0), :];
+        dat = :hCHO,
+        title = "Mixture predictions with \nsingle IgG fitted parameters",
+        R2pos = (0, -2.5),
+    )
+    pl_igg = plotAffinityViolin(c; murine = false, y_range = (5, 8))
+    rob1, rob2 = validateFittedKav(c; murine = false)
+
+    pp = plotGrid((4, 3), [nothing, nothing, raw_pred_pl, pl_noKav, pl1, pl2, pl_igg[1], pl_igg[2], pl_igg[3], pl_igg[4], rob1, rob2])
+    draw(PDF("figure2.pdf", 12inch, 12inch), pp)
+end
---
+++ src/figures/figureA3.jl
@@ -0,0 +1,35 @@
+""" Figure 3: fitted murine affinities """
+function figure3()
+    df = importMurineLeukocyte(; average = true)
+    Kav_old = importKavDist(; murine = true, regularKav = true, retdf = true)
+
+    ndf1 = predMix(df; Kav = Kav_old, Rtot = nothing, fs = [4, 33])
+    pl1 = plotPredvsMeasured(
+        ndf1;
+        xx = "Value",
+        yy = "Predict",
+        color = "ImCell",
+        shape = "Subclass",
+        R2pos = (0, -1.5),
+        title = "Raw murine leukocyte prediction\nwith documented affinities",
+    )
+
+    c_noKav = rungMCMC("leukNUTSfit_0524_KavOnly_v1.7.dat"; dat = :mLeuk, Kavd = Kav_old)
+    pl_noKav = plotMCMCPredict(
+        c_noKav,
+        df;
+        dat = :mLeuk,
+        Kav = Kav_old,
+        R2pos = (0, -2),
+        title = "Murine leukocyte prediction\nwith all but affinity fitting",
+    )
+
+    c = rungMCMC("leukNUTSfit_0524_v1.7.dat"; dat = :mLeuk)
+    pl2 = plotMCMCPredict(c, df; dat = :mLeuk, Kav = nothing, R2pos = (0, -2), title = "Murine leukocyte prediction\nwith updated affinities")
+
+    apls = plotAffinityViolin(c; murine = true, y_range = (4, 9))
+    #vpl1, vpl2 = validateFittedKav(c; murine = true)
+
+    pp = plotGrid((2, 3), [pl1, pl_noKav, pl2, apls[1], apls[2], apls[3]])
+    draw(PDF("figure3.pdf", 11inch, 6inch), pp)
+end
