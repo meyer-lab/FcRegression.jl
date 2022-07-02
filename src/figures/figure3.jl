54525667964c8a6a963bad17be3576b52376e65c
Cyrillus <cyztan@gmail.com>
2022-07-01 18:18:35 -0700

    Split old fig 2 into new fig 3 and 4

--- src/figures/figure3.jl
+++ src/figures/figure3.jl
@@ -1,1 +1,39 @@
 """ Figure 3: Explain model, fit data, and validate with Robinett """
+
+function figure3()
+    raw_predict = predMix(
+        averageMixData(loadMixData());
+        Kav = importKav(; murine = false, retdf = true),
+        Rtot = importRtotDist(:hCHO; regular = true, retdf = true),
+    )
+
+    raw_pred_pl = plotPredvsMeasured(
+        raw_predict;
+        xx = "Value",
+        xxlabel = "Measured",
+        color = "Receptor",
+        shape = "Valency",
+        title = "Raw model predictions without fitting",
+        R2pos = (0, -2.7),
+    )
+
+    df = loadMixData()
+    Kav_old = importKavDist(; murine = false, regularKav = true, retdf = true)
+    c_noKav = rungMCMC("humanNUTSfit_0517_KavOnly_v1.8.dat"; dat = :hCHO, Kavd = Kav_old)
+    pl_noKav = plotMCMCPredict(c_noKav, df; dat = :hCHO, Kav = Kav_old, R2pos = (0, -2), title = "Predictions with all but affinity fitting")
+
+    c = rungMCMC("humanNUTSfit_0505.dat"; dat = :hCHO, mcmc_iter = 1_000)
+
+    pl1 = plotMCMCPredict(c, df; dat = :hCHO, title = "All predictions with \nsingle hIgG fitted parameters", R2pos = (0, -2.5))
+    pl2 = plotMCMCPredict(
+        c,
+        df[(df."%_1" .!= 1.0) .& (df."%_2" .!= 1.0), :];
+        dat = :hCHO,
+        title = "Mixture predictions with \nsingle IgG fitted parameters",
+        R2pos = (0, -2.5),
+    )
+    rob1, rob2 = validateFittedKav(c; murine = false)
+
+    pp = plotGrid((3, 3), [nothing, nothing, raw_pred_pl, pl_noKav, pl1, pl2, rob1, rob2])
+    draw(PDF("figure3.pdf", 12inch, 10inch), pp)
+end
--- src/figures/figure4.jl
+++ src/figures/figure4.jl
@@ -5,3 +5,11 @@ Ideas:
 Fitted affinity vs. measured binding
 
 """
+
+function figure4()
+    c = rungMCMC("humanNUTSfit_0505.dat"; dat = :hCHO, mcmc_iter = 1_000)
+    pl_igg = plotAffinityViolin(c; murine = false, y_range = (5, 8))
+
+    pp = plotGrid((1, 4), [pl_igg[1], pl_igg[2], pl_igg[3], pl_igg[4]])
+    draw(PDF("figure4.pdf", 12inch, 3inch), pp)
+end
