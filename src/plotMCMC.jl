1cb6ef8a85eac2aef30367666b696dfa14ea7527
Cyrillus <cyztan@gmail.com>
2022-09-19 17:41:25 -0700

    Figure 4 updates: legend remove, FcγR name

--- src/plotMCMC.jl
+++ src/plotMCMC.jl
@@ -54,54 +54,6 @@ function plotPredvsMeasured(
     )
 end
 
-""" Making one single violin plot. Called by plotAffinityViolin() """
-function plot_distribution_violins(
-    df::AbstractDataFrame,
-    dist_list::Vector{T};
-    title = "",
-    x_name = "Receptor",
-    y_range = (5, 8),
-) where {T <: Distribution}
-    setGadflyTheme()
-    @assert size(df)[2] == length(dist_list)
-
-    odf = DataFrame([names(df)[i] => rand(dist_list[i], 100_000) for i = 1:size(df)[2] if dist_list[i].μ > 0])
-    df = stack(df, variable_name = x_name, value_name = "Value")
-    odf = stack(odf, variable_name = x_name, value_name = "Value")
-
-    return plot(
-        layer(df, x = x_name, y = "Value", Geom.violin, Theme(default_color = colorant"firebrick4")),
-        layer(odf, x = x_name, y = "Value", Geom.violin, Theme(default_color = colorant"navajowhite2")),
-        Coord.cartesian(ymin = y_range[1], ymax = y_range[2]),
-        Scale.y_log10,
-        Guide.manual_color_key("Legend", ["Prior", "Posterior"], ["navajowhite2", "firebrick4"]),
-        Guide.ylabel("<i>K</i><sub>a</sub> (M<sup>-1</sup>)"),
-        Guide.title(title),
-    )
-end
-
-""" Make violin plot of affinities from a chain """
-function plotAffinityViolin(c::Chains; murine::Bool, y_range = (4, 9))
-    pref = murine ? "m" : "h"
-    Kav_priors = importKavDist(; murine = murine, retdf = true)
-
-    len = length(Matrix(Kav_priors[!, Not("IgG")]))
-    Kav_posts = deepcopy(Kav_priors)
-    Kav = [c["Kav[$i]"].data for i = 1:len]
-    Kav_posts[!, Not("IgG")] = typeof(Kav[1, 1]).(reshape(Kav, size(Kav_posts)[1], :))
-
-    pls = Vector{Union{Gadfly.Plot, Context}}(undef, size(Kav_priors)[1])
-    for (ii, igg) in enumerate(Kav_priors[!, "IgG"])
-        priors = reshape(Matrix(Kav_priors[Kav_priors."IgG" .== igg, Not("IgG")]), :)
-        posts = DataFrame(
-            hcat([reshape(Kav_posts[Kav_posts."IgG" .== igg, Not("IgG")][1, i], :) for i = 1:(size(Kav_posts)[2] - 1)]...),
-            names(Kav_posts)[2:end],
-        )
-        pls[ii] = plot_distribution_violins(posts, priors; y_range = y_range, title = "$pref$igg Affinities Distributions")
-    end
-    return pls
-end
-
 
 """ Making a single subplot for priors and posteriors. Called by plotMCMCdists() """
 function plot_dist_histogram(dat::Array{Float64}, dist::Distribution, name::String = ""; bincount = 20)
