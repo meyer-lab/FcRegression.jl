cf7917e0f62abe2e3bba18d664a89c88d2948c29
Cyrillus <cyztan@gmail.com>
2022-12-22 17:45:21 -0800

    MCMC for FcγRIIIB affinity fitting

--- src/CD16b.jl
+++ src/CD16b.jl
@@ -1,5 +1,5 @@
 function importCD16b()
-    df = CSV.File(joinpath(FcRegression.dataDir, "dec2022-FcgR3b-binding.csv"), delim = ",", comment = "#") |> DataFrame
+    df = CSV.File(joinpath(dataDir, "dec2022-FcgR3b-binding.csv"), delim = ",", comment = "#") |> DataFrame
     df = stack(df, Not(["Valency", "Cell", "Subclass"]), variable_name = "Experiment", value_name = "Value")
     df = dropmissing(df)
     df[!, "Value"] = convert.(Float64, df[!, "Value"])
@@ -18,7 +18,53 @@ function importCD16b()
     return df
 end
 
-function inferCD16b()
+@model function gmodelCD16b(df, values)
     CD16bRtot = Dict("FcgRIIIB-NA1" => 100_000.0, "FcgRIIIB-NA2" => 100_000.0)
-    pred = FcRegression.predMix(df; Kav = Kav[!, [1,8,9]], Rtot = CD16bRtot)
+
+    # sample Kav
+    Kavd = FcRegression.importKavDist(; murine = false, regularKav = true, retdf = true)[:, [1,8,9]]
+    Kav_dist = FcRegression.importKavDist(; murine = false, regularKav = false, retdf=false)[:, [7,8]]
+    Kav = Matrix(undef, size(Kav_dist)...)
+    for ii in eachindex(Kav)
+        Kav[ii] ~ Kav_dist[ii]
+    end
+    Kavd[!, Not("IgG")] = typeof(Kav[1, 1]).(Kav)
+
+    # sample f4, f33, KxStar
+    f4 ~ f4Dist
+    f33 ~ f33Dist
+    KxStar ~ KxStarDist
+
+    # fit predictions
+    if all(0.0 .<= Matrix(Kavd[!, Not("IgG")]) .< Inf) && all(0.0 .< [f4, f33, KxStar] .< Inf)
+        df = predMix(deepcopy(df); Rtot = CD16bRtot, Kav = Kavd, KxStar = KxStar, fs = [f4, f33])
+    else
+        df = deepcopy(df)
+        df."Predict" .= Inf
+    end
+
+    stdv = std(log.(df."Predict") - log.(values))
+    values ~ MvLogNormal(log.(df."Predict"), stdv * I)
+    nothing
 end
+
+function inferCD16b(; mcmc_iter = 1000)
+    df = importCD16b()
+    m = gmodelCD16b(df, df."Value")
+    opts = Optim.Options(iterations = 500, show_every = 10, show_trace = true)
+    opt = optimize(m, MAP(), LBFGS(; m = 20), opts)
+    c = sample(m, NUTS(), mcmc_iter, init_params = opt.values.array)
+
+    Kavd = FcRegression.importKavDist(; murine = false, regularKav = true, retdf = true)[:, [1,8,9]]
+    Kav = [median(c["Kav[$i]"].data) for i = 1:8]
+    Kavd[!, Not("IgG")] = typeof(Kav[1, 1]).(reshape(Kav, size(Kavd)[1], :))
+end
+
+"""
+ Row │ IgG      FcgRIIIB-NA1   FcgRIIIB-NA2        
+─────┼───────────────────────────────────────
+   1 │ IgG1         3.47807e5      3.75184e5
+   2 │ IgG2     18524.8        25543.6
+   3 │ IgG3         9.80651e5      8.82411e5
+   4 │ IgG4      9481.83       21827.7
+"""
