dccd56e4093d775fafaed89f47b85fcf9bcac7c7
Cyrillus Tan <cyztan@gmail.com>
2023-02-16 00:38:42 -0800

    Remove the FcRegression prefix (#544)

--- src/figures/figure6.jl
+++ src/figures/figure6.jl
@@ -1,7 +1,7 @@
 """ Plot in vivo regression results. Return predictions, cell weights, and receptor weights for MAP and MCMC. """
 function plot_regressions(df; Kav::DataFrame, murine = false, cellTypes = nothing, ptitle = "")
     opt, optcv, cdf = runRegMAP(df; murine = murine, Kav = Kav, cellTypes = cellTypes)
-    c0, ccdf0 = FcRegression.runRegMCMC(df; murine = murine, Kav = Kav0, mcmc_iter = 200, cellTypes = cellTypes)
+    c0, ccdf0 = runRegMCMC(df; murine = murine, Kav = Kav0, mcmc_iter = 200, cellTypes = cellTypes)
 
     pl_map = plotRegMCMC(opt, deepcopy(df); ptitle = ptitle * "[MAP]", Kav = Kav, cellTypes = cellTypes, colorL = "Genotype", shapeL = "Condition")
     pl_mcmc = plotRegMCMC(c, deepcopy(df); ptitle = ptitle * "[MCMC]", Kav = Kav, cellTypes = cellTypes, colorL = "Genotype", shapeL = "Condition")
@@ -13,12 +13,12 @@ end
 
 function figure6(ssize = (8.5inch, 5.5inch); cellTypes = ["ncMO", "cMO", "Neu"], mcmc_iter = 50000, suffix = "0117_", kwargs...)
     setGadflyTheme()
-    df = FcRegression.importHumanized("ITP")
+    df = importHumanized("ITP")
 
-    Kav0 = FcRegression.extractNewHumanKav(; old = true)
-    Kav1 = FcRegression.extractNewHumanKav(; old = false)
+    Kav0 = extractNewHumanKav(; old = true)
+    Kav1 = extractNewHumanKav(; old = false)
 
-    c1, ccdf1 = FcRegression.runRegMCMC(
+    c1, ccdf1 = runRegMCMC(
         df,
         "regMCMC_$(suffix)1.dat";
         murine = false,
@@ -27,7 +27,7 @@ function figure6(ssize = (8.5inch, 5.5inch); cellTypes = ["ncMO", "cMO", "Neu"],
         mcmc_iter = mcmc_iter,
         cellTypes = cellTypes,
     )
-    c0, ccdf0 = FcRegression.runRegMCMC(
+    c0, ccdf0 = runRegMCMC(
         df,
         "regMCMC_$(suffix)0.dat";
         murine = false,
@@ -40,7 +40,7 @@ function figure6(ssize = (8.5inch, 5.5inch); cellTypes = ["ncMO", "cMO", "Neu"],
     c0 = c0[(mcmc_iter ÷ 10 * 7):end]
     c1 = c1[(mcmc_iter ÷ 10 * 7):end]
 
-    pl_map0 = FcRegression.plotRegMCMC(
+    pl_map0 = plotRegMCMC(
         c0,
         deepcopy(df);
         ptitle = "documented affinities",
@@ -51,9 +51,9 @@ function figure6(ssize = (8.5inch, 5.5inch); cellTypes = ["ncMO", "cMO", "Neu"],
         cellTypes = cellTypes,
     )
     cell_map0, act_map0 =
-        FcRegression.plotRegParams(c0; ptitle = "documented affinities", legend = false, Kav = Kav0, cellTypes = cellTypes, cell_max_y = 1.5)
+        plotRegParams(c0; ptitle = "documented affinities", legend = false, Kav = Kav0, cellTypes = cellTypes, cell_max_y = 1.5)
 
-    pl_map1 = FcRegression.plotRegMCMC(
+    pl_map1 = plotRegMCMC(
         c1,
         deepcopy(df);
         ptitle = "updated affinities",
@@ -64,9 +64,9 @@ function figure6(ssize = (8.5inch, 5.5inch); cellTypes = ["ncMO", "cMO", "Neu"],
         cellTypes = cellTypes,
     )
     cell_map1, act_map1 =
-        FcRegression.plotRegParams(c1; ptitle = "updated affinities", legend = true, Kav = Kav1, cellTypes = cellTypes, cell_max_y = 1.5)
+        plotRegParams(c1; ptitle = "updated affinities", legend = true, Kav = Kav1, cellTypes = cellTypes, cell_max_y = 1.5)
 
-    pl_mapL = FcRegression.plotRegMCMC(
+    pl_mapL = plotRegMCMC(
         c1,
         deepcopy(df);
         ptitle = "updated affinities",
@@ -76,9 +76,9 @@ function figure6(ssize = (8.5inch, 5.5inch); cellTypes = ["ncMO", "cMO", "Neu"],
         Kav = Kav1,
         cellTypes = cellTypes,
     )
-    cell_mapL, act_mapL = FcRegression.plotRegParams(c1; ptitle = "updated affinities", legend = true, Kav = Kav1, cellTypes = cellTypes)
+    cell_mapL, act_mapL = plotRegParams(c1; ptitle = "updated affinities", legend = true, Kav = Kav1, cellTypes = cellTypes)
 
-    pl = FcRegression.plotGrid(
+    pl = plotGrid(
         (2, 3),
         [nothing, pl_map0, pl_map1, nothing, cell_map0, cell_map1];
         sublabels = "abc de",
