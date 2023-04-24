7bbe1a2f52773563af6ee887c6e822da6896e5ff
ctan <cyztan@gmail.com>
2023-04-24 02:54:39 -0700

    Remove unused cache files; remove synergy related  plots

--- src/figures/figure3.jl
+++ src/figures/figure3.jl
@@ -78,11 +78,11 @@ function figure3(ssize = (10inch, 8inch); kwargs...)
 
     df = loadMixData()
     Kav_old = importKavDist(; murine = false, regularKav = true, retdf = true)
-    c_noKav = rungMCMC("humanfit_0701_noKav.dat"; dat = :hCHO, Kavd = Kav_old)
+    c_noKav = rungMCMC("fit_mixture_no_Kav.dat"; dat = :hCHO, Kavd = Kav_old)
     pl_noKav = plotMCMCPredict(c_noKav, df; dat = :hCHO, Kav = Kav_old, R2pos = (0, -2), 
         title = "Predictions with all but affinity fitting", legend = false)
 
-    c = rungMCMC("humanKavfit_0701.dat"; dat = :hCHO, mcmc_iter = 1_000)
+    c = rungMCMC("fit_mixture.dat"; dat = :hCHO, mcmc_iter = 1_000)
     pl1 = plotMCMCPredict(
         c,
         df;
