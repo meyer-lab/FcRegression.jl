d1f500ad86e4fb64494bd21feb8c752629f2a5b4
ctan <cyztan@gmail.com>
2023-04-24 03:17:10 -0700

    Update tests

--- test/testDataHelpers.jl
+++ test/testDataHelpers.jl
@@ -19,7 +19,6 @@
         @test size(FcRegression.importKav(murine = false, retdf = false), 2) == length(FcRegression.humanFcgR)
         @test FcRegression.importKav(; murine = true, retdf = true) isa DataFrame
         @test FcRegression.importKav(; murine = false, retdf = true) isa DataFrame
-        @test FcRegression.importDeplExp() isa DataFrame
     end
 
     @testset "Testing human FcgR abundance import for genotypes" begin
@@ -34,18 +33,14 @@
 
         ## Assert correct assigning of the genotypes (can add reverse cases too)
         @test(arr_HIV[d["FcgRIIA-131H"], :] == arr_RTF[d["FcgRIIA-131R"], :] && arr_HIV[d["FcgRIIA-131R"], :] == arr_RTF[d["FcgRIIA-131H"], :])
-        #@test(arr_HIV[d["FcgRIIB-232I"], :] == arr_RTF[d["FcgRIIB-232T"], :] && arr_HIV[d["FcgRIIB-232T"], :] == arr_RTF[d["FcgRIIB-232I"], :])
         @test(arr_HIV[d["FcgRIIIA-158V"], :] == arr_RTF[d["FcgRIIIA-158F"], :] && arr_HIV[d["FcgRIIIA-158F"], :] == arr_RTF[d["FcgRIIIA-158V"], :])
 
         ## Check Heterozygote Case
         @test(arr_RTF[d["FcgRIIA-131R"], :] == arr_heterozygote[d["FcgRIIA-131H"], :] .+ arr_heterozygote[d["FcgRIIA-131R"], :])
-        #@test(arr_HIV[d["FcgRIIB-232I"], :] == arr_heterozygote[d["FcgRIIB-232I"], :] .+ arr_heterozygote[d["FcgRIIB-232T"], :])
         @test(arr_RTF[d["FcgRIIIA-158F"], :] == arr_heterozygote[d["FcgRIIIA-158V"], :] .+ arr_heterozygote[d["FcgRIIIA-158F"], :])
 
         ## Check mixed genotype case
         @test(arr_HZF[d["FcgRIIA-131H"], :] == arr_HIV[d["FcgRIIA-131H"], :])
-        #@test(arr_HZF[d["FcgRIIB-232I"], :] == arr_HZF[d["FcgRIIB-232T"], :])
-        #@test(arr_RTF[d["FcgRIIB-232T"], :] == arr_HZF[d["FcgRIIB-232I"], :] .+ arr_HZF[d["FcgRIIB-232T"], :])
     end
 
 end
